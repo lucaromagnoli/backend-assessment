40c0412ad4852077720ccd89f07220d471b4e7a3
Luca Romagnoli <luca.romagnoli@justeattakeaway.com>
2022-02-08 14:01:23 +0000

    implement Fifth and tests

--- tests/test_problems.py
+++ tests/test_problems.py
@@ -2,6 +2,24 @@ import pytest
 
 from turner_and_townsend import problems
 
+
+@pytest.fixture
+def stack_with_data():
+    return problems.Stack([1, 2])
+
+
+@pytest.fixture
+def interpreter():
+    return problems.FifthInterpreter()
+
+
+@pytest.fixture
+def interpreter_with_data(interpreter):
+    interpreter.parse_input_line('PUSH 1')
+    interpreter.parse_input_line('PUSH 2')
+    return interpreter
+
+
 collatz_conjecture_test_data = [
     (1, 0),
     (2, 1),
@@ -36,3 +54,136 @@ roman_to_integer_test_data = [
 @pytest.mark.parametrize('n, expected', roman_to_integer_test_data)
 def test_roman_to_integer(n, expected):
     assert problems.roman_to_integer(n) == expected
+
+
+def test_stack_add(stack_with_data):
+    stack_with_data.add()
+    assert stack_with_data == [3]
+
+
+def test_stack_sub(stack_with_data):
+    stack_with_data.sub()
+    assert stack_with_data == [1]
+
+
+def test_stack_mul(stack_with_data):
+    stack_with_data.mul()
+    assert stack_with_data == [2]
+
+
+def test_stack_div(stack_with_data):
+    stack_with_data.div()
+    assert stack_with_data == [2]
+
+
+def test_stack_pop(stack_with_data):
+    stack_with_data.pop()
+    assert stack_with_data == [1]
+
+
+def test_stack_push(stack_with_data):
+    stack_with_data.push(3)
+    assert stack_with_data == [1, 2, 3]
+
+
+def test_stack_swap(stack_with_data):
+    stack_with_data.swap()
+    assert stack_with_data == [2, 1]
+
+
+def test_stack_dup(stack_with_data):
+    stack_with_data.dup()
+    assert stack_with_data == [1, 2, 2]
+
+
+def test_stack_arithmetic_exception():
+    stack = problems.Stack()
+    with pytest.raises(ValueError):
+        stack.add()
+
+
+def test_fifth_interpreter_push(interpreter):
+    interpreter.parse_input_line('PUSH 1')
+    assert interpreter.stack == [1]
+    interpreter.parse_input_line('PUSH 2')
+    assert interpreter.stack == [1, 2]
+
+
+def test_fifth_interpreter_add(interpreter_with_data):
+    interpreter_with_data.parse_input_line('+')
+    assert interpreter_with_data.stack == [3]
+
+
+def test_fifth_interpreter_sub(interpreter_with_data):
+    interpreter_with_data.parse_input_line('-')
+    assert interpreter_with_data.stack == [1]
+
+
+def test_fifth_interpreter_mul(interpreter_with_data):
+    interpreter_with_data.parse_input_line('*')
+    assert interpreter_with_data.stack == [2]
+
+
+def test_fifth_interpreter_div(interpreter_with_data):
+    interpreter_with_data.parse_input_line('/')
+    assert interpreter_with_data.stack == [2]
+
+
+def test_fifth_interpreter_pop(interpreter_with_data):
+    interpreter_with_data.parse_input_line('POP')
+    assert interpreter_with_data.stack == [1]
+
+
+def test_fifth_interpreter_swap(interpreter_with_data):
+    interpreter_with_data.parse_input_line('SWAP')
+    assert interpreter_with_data.stack == [2, 1]
+
+
+def test_fifth_interpreter_dup(interpreter_with_data):
+    interpreter_with_data.parse_input_line('DUP')
+    assert interpreter_with_data.stack == [1, 2, 2]
+
+
+def test_fifth_interpreter_exceptions_pop(interpreter):
+    with pytest.raises(problems.FifthError):
+        interpreter.parse_input_line('POP')
+
+
+def test_fifth_interpreter_exceptions_push(interpreter):
+    with pytest.raises(problems.FifthError):
+        interpreter.parse_input_line('PUSH hello')
+
+
+def test_fifth_interpreter_exceptions_swap(interpreter):
+    with pytest.raises(problems.FifthError):
+        interpreter.parse_input_line('DUP')
+
+
+def test_fifth_interpreter_exceptions_dup(interpreter):
+    interpreter.stack.push(1)
+    with pytest.raises(problems.FifthError):
+        interpreter.parse_input_line('SWAP')
+
+
+def test_fifth_interpreter_exceptions_add(interpreter):
+    interpreter.stack.push(1)
+    with pytest.raises(problems.FifthError):
+        interpreter.parse_input_line('+')
+
+
+def test_fifth_interpreter_exceptions_sub(interpreter):
+    interpreter.stack.push(1)
+    with pytest.raises(problems.FifthError):
+        interpreter.parse_input_line('-')
+
+
+def test_fifth_interpreter_exceptions_mul(interpreter):
+    interpreter.stack.push(1)
+    with pytest.raises(problems.FifthError):
+        interpreter.parse_input_line('*')
+
+
+def test_fifth_interpreter_exceptions_div(interpreter):
+    interpreter.stack.push(1)
+    with pytest.raises(problems.FifthError):
+        interpreter.parse_input_line('/')
--- turner_and_townsend/problems.py
+++ turner_and_townsend/problems.py
@@ -1,3 +1,6 @@
+from typing import Optional, List
+
+
 def collatz_conjecture(n: int) -> int:
     """Given a numeric input, count how many steps it takes until the Collatz sequence reaches 1.
 
@@ -44,7 +47,116 @@ class RomanNumber:
         return sum(self.numbers_map[c] for c in self.value)
 
 
-def roman_to_integer(string):
+def roman_to_integer(string: str) -> int:
     """Convert a Roman number string to integer"""
     roman = RomanNumber(string)
     return roman.to_integer()
+
+
+class Stack:
+    """Stack implementation using list"""
+    def __init__(self, stack: Optional[List] = None):
+        if stack is None:
+            self.stack = []
+        else:
+            self.stack = stack
+
+    def __str__(self):
+        return f'{self.stack}'
+
+    def __eq__(self, other):
+        return self.stack == other
+
+    def add(self):
+        self.check_stack_for_operation('+')
+        first = self.pop()
+        second = self.pop()
+        self.push(first + second)
+
+    def sub(self):
+        self.check_stack_for_operation('-')
+        first = self.pop()
+        second = self.pop()
+        self.push(first - second)
+
+    def mul(self):
+        self.check_stack_for_operation('*')
+        first = self.pop()
+        second = self.pop()
+        self.push(first * second)
+
+    def div(self):
+        self.check_stack_for_operation('/')
+        first = self.pop()
+        second = self.pop()
+        value = int(first/second)
+        self.push(value)
+
+    def check_stack_for_operation(self, operation):
+        if len(self.stack) < 2:
+            raise ValueError(f'Cannot apply {operation} operation to a stack with less than two elements')
+
+    def pop(self):
+        return self.stack.pop()
+
+    def push(self, n):
+        return self.stack.append(n)
+
+    def swap(self):
+        first = self.pop()
+        second = self.pop()
+        self.push(first)
+        self.push(second)
+
+    def dup(self):
+        self.push(self.stack[-1])
+
+
+class FifthError(ValueError):
+    pass
+
+
+class FifthInterpreter:
+    ops_map = {
+        '+': 'add',
+        '-': 'sub',
+        '*': 'mul',
+        '/': 'div',
+        'POP': 'pop',
+        'SWAP': 'swap',
+        'DUP': 'dup'
+    }
+
+    def __init__(self):
+        self.stack = Stack()
+
+    def _unknown_operation_exception(self, command):
+        """Raise value error if command is unknown"""
+        raise ValueError(f'Unknown operation: {command}')
+
+    def parse_input_line(self, string_input):
+        if string_input.startswith('PUSH'):
+            self._parse_push_line(string_input)
+        else:
+            self._execute_operation(string_input)
+
+    def _parse_push_line(self, string_input):
+        operation, value = string_input.split()
+        if operation != 'PUSH':
+            self._unknown_operation_exception(operation)
+        try:
+            value = int(value)
+        except ValueError as e:
+            raise FifthError(str(e))
+        self.stack.push(value)
+
+    def _execute_operation(self, string_input):
+        if string_input in self.ops_map:
+            op_str = self.ops_map[string_input]
+            op = getattr(self.stack, op_str)
+            try:
+                op()
+            except (IndexError, ValueError) as e:
+                raise FifthError(str(e))
+        else:
+            self._unknown_operation_exception(string_input)
